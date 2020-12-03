305b07f4b0f4318d7d610281fd39406073ddeee6
Roger Rengifo <rogerrengifo@Rogers-MacBook-Pro.local>
2020-12-03 14:11:53 -0500

    fixing new currencyExchanges

--- src/containers/CurrencyExchange/index.jsx
+++ src/containers/CurrencyExchange/index.jsx
@@ -54,8 +54,7 @@ const CurrencyExchange = (props) => {
               exchangeRateId={exchangeRateId}
             />
           </div>
-          {/* <img src={step === 4 ? ExchangeSuccess : ExchangeSteps} className={classes.ExchangeImg} alt='cambio de divisas' /> */}
-          <p>Aqui va una imagen si?</p>
+          <img src={step === 4 ? ExchangeSuccess : ExchangeSteps} className={classes.ExchangeImg} alt='cambio de divisas' />
         </div>
       </main>
       <Modal animationClassName='slide-up-down'>
